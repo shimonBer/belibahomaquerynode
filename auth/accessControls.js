72d498431f16064f377e124783dc1a6aa08b6620
shimonBer <shimon.ber@gmail.com>
2020-03-10 19:17:48 +0200

    organize-files

--- auth/accessControls.js
+++ auth/accessControls.js
@@ -10,12 +10,13 @@ module.exports = function(req, res, next) {
   
     // Request headers you wish to allow
     res.setHeader(
-      "Access-Control-Allow-Headers", "X-Requested-With, Content-Type, x-auth-token"
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type,x-auth-token"
     );
   
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    // res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Credentials", true);
   
     // Pass to next layer of middleware
     next();
