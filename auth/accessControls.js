cb614c8fd368b0e9655f824e59340246d1b9416d
shimonBer <shimon.ber@gmail.com>
2021-12-07 19:09:26 +0200

    h

--- auth/accessControls.js
+++ auth/accessControls.js
@@ -25,7 +25,6 @@ module.exports = function (req, res, next) {
     // to the API (e.g. in case you use sessions)
     res.setHeader("Access-Control-Allow-Credentials", true)
 
-    
     // Pass to next layer of middleware
     next()
 }
